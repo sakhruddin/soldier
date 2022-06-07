ec837ad6213a7979257dfa075f069d0855ad57ba
sakhrudin <magnat0610@gmail.com>
2022-06-07 20:21:17 +0300

    завершил игру и отформатировал

--- soldier.js
+++ soldier.js
@@ -2,38 +2,37 @@ const soldier = {
     name: 'Халк',
     health: 10,
     weapon: {
-      weaponName: 'ПП',
-      cartridges: 30, // патроны
+        weaponName: 'ПП',
+        cartridges: 30, // патроны
     },
     supplies: 3, // припасы
-  
-    gun: function () { 
-        if(this.weapon.cartridges === 0){
-        console.log('обойма пуста. перезарядить')
+
+    gun: function () {
+        if (this.weapon.cartridges === 0) {
+            console.log('обойма пуста. перезарядить')
         }
         this.weapon.cartridges--
-     console.log('бах-бах')
-  },
-  
-  
-    
-    
-    reloading: function () {
-      if(this.supplies === 0){
-        console.log('не осталось патронов')
-      }else { 
-        this.weapon.cartridges = 30;
-        this.supplies--
-        return ` перезарядка...`
-      }
+        console.log('бах-бах')
     },
-    
-    wound: function () {
-        if(this.health === 0){
-          console.log('you loses')
+
+
+
+
+    reloading: function () {
+        if (this.supplies === 0) {
+            console.log('не осталось патронов')
         } else {
-         this.health--
+            this.weapon.cartridges = 30;
+            this.supplies--
+            return ` перезарядка...`
         }
-  };
-  soldier.reloading()
-  console.log(soldier)
+    },
+
+    wound: function () {
+        if (this.health === 0) {
+            console.log('you loses')
+        } else {
+            this.health--
+        }
+    }
+}
