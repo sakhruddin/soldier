1f495f4c0138870644f3b4fb235bcb1bd015cf50
sakhrudin <magnat0610@gmail.com>
2022-06-07 19:04:53 +0300

    задал осноные параметры в игре

---
+++ soldier.js
@@ -0,0 +1,13 @@
+const soldier = {
+    name: 'Халк',
+    health: 10,
+    weapon: {
+      weaponName: 'ПП',
+      cartridges: 30, // патроны
+    },
+    supplies: 3, // припасы
+  
+    gun: function () {}, // стрелять 
+    reloading: function () {}, // перезарядить
+    wound: function () {}, // ранить
+  };
